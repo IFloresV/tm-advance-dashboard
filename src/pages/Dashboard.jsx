bdd52f5fe5447f0c2936eec329a683fd16e2ec83
PROGRAMADOR EXTERNO <programador_externo@zermat.com.mx>
2025-04-29 20:56:49 -0600

    Feat - Header, configuracion de tailwind y vite

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+// import UnitList from "../components/UnitList";
+// import UnitMap from "../components/UnitMap";
+// import MetricsCards from "../components/MetricsCards";
+// import AlertsChart from "../components/AlertsChart";
+
+const Dashboard = () => {
+   //    const [selectedUnit, setSelectedUnit] = useState(null);
+
+   return <div className=""></div>;
+};
+
+export default Dashboard;
